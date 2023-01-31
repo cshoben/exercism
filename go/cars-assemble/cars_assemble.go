f1ef6e0fabd889146b513382144cfc267af04c7a
cshoben <cshoben@users.noreply.github.com>
2023-01-30 19:36:30 -0500

    cars_assemble written and passes tests

--- go/cars-assemble/cars_assemble.go
+++ go/cars-assemble/cars_assemble.go
@@ -3,7 +3,6 @@ package cars
 // CalculateWorkingCarsPerHour calculates how many working cars are
 // produced by the assembly line every hour.
 func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
-	//panic("CalculateWorkingCarsPerHour not implemented")
 	var cars float64
 	var percentRate = successRate/100
 	cars = float64(productionRate) * percentRate
@@ -13,14 +12,21 @@ func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float6
 // CalculateWorkingCarsPerMinute calculates how many working cars are
 // produced by the assembly line every minute.
 func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
-	panic("CalculateWorkingCarsPerMinute not implemented")
-	var cars float64
-	var minRate = productionRate/60
-	cars = float64(minRate) * successRate
-	return int(cars)
+	var carsPerMin int
+	var carsPerHour float64
+	carsPerHour = CalculateWorkingCarsPerHour(productionRate, successRate)
+	carsPerMin = int(carsPerHour)/60
+	return carsPerMin
 }
 
 // CalculateCost works out the cost of producing the given number of cars.
 func CalculateCost(carsCount int) uint {
-	panic("CalculateCost not implemented")
+	var remainder int
+	var costGroupsOfTen uint
+	var costIndividualBuilds uint
+
+	costGroupsOfTen = uint((carsCount/10) * 95000)
+	remainder = carsCount%10
+	costIndividualBuilds = uint(remainder*10000)
+	return costGroupsOfTen + costIndividualBuilds
 }

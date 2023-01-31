f011f686f007f500c4ada1aed0c7f4d80801a360
cshoben <cshoben@users.noreply.github.com>
2023-01-30 20:17:21 -0500

    working out the stars based on input int

--- go/welcome-to-tech-palace/welcome_to_tech_palace.go
+++ go/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -1,13 +1,18 @@
 package techpalace
 
+import "strings"
+
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
-	panic("Please implement the WelcomeMessage() function")
+	welcome := "Welcom to the Tech Palace, "
+	return welcome + customer
 }
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-	panic("Please implement the AddBorder() function")
+	var stars string
+	stars = "*"
+	stars = "*" * numStarsPerLine
 }
 
 // CleanupMessage cleans up an old marketing message.

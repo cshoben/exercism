80dc1a6624e05faed56ff75accf31f0c29697991
cshoben <cshoben@users.noreply.github.com>
2023-01-31 22:31:35 -0500

    all functions welcome-to-tech-palace passing tests

--- go/welcome-to-tech-palace/welcome_to_tech_palace.go
+++ go/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -4,18 +4,20 @@ import "strings"
 
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
-	welcome := "Welcom to the Tech Palace, "
-	return welcome + customer
+	welcome := "Welcome to the Tech Palace, "
+	return welcome + strings.ToUpper(customer)
 }
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
 	var stars string
-	stars = "*"
-	stars = "*" * numStarsPerLine
+	stars = strings.Repeat("*", numStarsPerLine)
+	return stars + "\n" + welcomeMsg + "\n" + stars
 }
 
 // CleanupMessage cleans up an old marketing message.
 func CleanupMessage(oldMsg string) string {
-	panic("Please implement the CleanupMessage() function")
+	remStarNewLine := strings.Trim(oldMsg, "*\n")
+	removeSpace := strings.TrimSpace(remStarNewLine)
+	return removeSpace
 }
